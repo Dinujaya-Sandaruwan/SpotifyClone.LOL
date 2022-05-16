667cabd59eee14e354cc75ef51d63f42b0cd607c
Dinujaya Sandaruwan <itkindom.dhomes@gmail.com>
2022-05-16 10:20:42 +0530

    Finished For Day 01

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,17 @@
 // Instantiating the global app object
 // var app = {};
+
+// let topp = document.getElementById('top');
+// let round = document.getElementById('round');
+
+// round.addEventListener('mouseover',test);
+// round.addEventListener('mouseleave',test1);
+
+// function test() {
+//     topp.style.backgroundImage = 'linear-gradient(to bottom, var(--white) 0%, var(--bg-color) 80%)';
+    
+// }
+// function test1() {
+//     topp.style.backgroundImage = 'linear-gradient(to bottom, var(--green) 0%, var(--bg-color) 80%)';
+    
+// }
