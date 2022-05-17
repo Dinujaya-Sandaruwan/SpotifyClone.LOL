bc52bc5d852ec63d3a6ec00346cb3d27c6320a68
Dinujaya Sandaruwan <itkindom.dhomes@gmail.com>
2022-05-18 02:32:19 +0530

    Finished For 05.17

---
+++ app/js/_song-fn.js
@@ -0,0 +1,401 @@
+function mxp_00(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[0].music);
+        loadTrack(0)
+        top_01.setAttribute('name', 'pause');
+        track_name.innerText = music_list[0].name;
+        track_artist.innerText = music_list[0].artist;
+        track_art.src = music_list[0].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_01;
+        element_name = element_id.id;
+    } else {
+        top_01.setAttribute('name', 'play');
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_01') {
+            curr_track = new Audio(music_list[0].music);
+            loadTrack(0)
+            top_01.setAttribute('name', 'pause');
+            track_name.innerText = music_list[0].name;
+            track_artist.innerText = music_list[0].artist;
+            track_art.src = music_list[0].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_01;
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_01(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[1].music);
+        loadTrack(1)
+        top_02.setAttribute('name', 'pause');
+        track_name.innerText = music_list[1].name;
+        track_artist.innerText = music_list[1].artist;
+        track_art.src = music_list[1].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_02;
+        element_name = element_id.id
+    } else {
+        top_02.setAttribute('name', 'play');
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_02') {
+            curr_track = new Audio(music_list[1].music);
+            loadTrack(1)
+            top_02.setAttribute('name', 'pause');
+            track_name.innerText = music_list[1].name;
+            track_artist.innerText = music_list[1].artist;
+            track_art.src = music_list[1].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_02;
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_02(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[2].music);
+        loadTrack(2)
+        top_03.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[2].name;
+        track_artist.innerText = music_list[2].artist;
+        track_art.src = music_list[2].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_03; //change
+        element_name = element_id.id
+    } else {
+        top_03.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_03') { //change
+            curr_track = new Audio(music_list[2].music);
+            loadTrack(2)
+            top_03.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[2].name;
+            track_artist.innerText = music_list[2].artist;
+            track_art.src = music_list[2].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_03;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_03(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[3].music);
+        loadTrack(3)
+        top_04.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[3].name;
+        track_artist.innerText = music_list[3].artist;
+        track_art.src = music_list[3].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_04; //change
+        element_name = element_id.id
+    } else {
+        top_04.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_04') { //change
+            curr_track = new Audio(music_list[3].music);
+            loadTrack(3)
+            top_04.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[3].name;
+            track_artist.innerText = music_list[3].artist;
+            track_art.src = music_list[3].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_04;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_04(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[4].music);
+        loadTrack(4)
+        top_05.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[4].name;
+        track_artist.innerText = music_list[4].artist;
+        track_art.src = music_list[4].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_05; //change
+        element_name = element_id.id
+    } else {
+        top_05.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_05') { //change
+            curr_track = new Audio(music_list[4].music);
+            loadTrack(4)
+            top_05.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[4].name;
+            track_artist.innerText = music_list[4].artist;
+            track_art.src = music_list[4].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_05;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_05(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[5].music);
+        loadTrack(5)
+        top_06.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[5].name;
+        track_artist.innerText = music_list[5].artist;
+        track_art.src = music_list[5].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = top_06; //change
+        element_name = element_id.id
+    } else {
+        top_06.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'top_06') { //change
+            curr_track = new Audio(music_list[5].music);
+            loadTrack(5)
+            top_06.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[5].name;
+            track_artist.innerText = music_list[5].artist;
+            track_art.src = music_list[5].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = top_06;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_06(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[6].music);
+        loadTrack(6)
+        mx_01.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[6].name;
+        track_artist.innerText = music_list[6].artist;
+        track_art.src = music_list[6].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_01; //change
+        element_name = element_id.id
+    } else {
+        mx_01.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx1') { //change
+            curr_track = new Audio(music_list[6].music);
+            loadTrack(6)
+            mx_01.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[6].name;
+            track_artist.innerText = music_list[6].artist;
+            track_art.src = music_list[6].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_01;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+
+
+
+
+
+
+function mxp_07(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[7].music);
+        loadTrack(7)
+        mx_02.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[7].name;
+        track_artist.innerText = music_list[7].artist;
+        track_art.src = music_list[7].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_02; //change
+        element_name = element_id.id
+    } else {
+        mx_02.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx2') { //change
+            curr_track = new Audio(music_list[7].music);
+            loadTrack(7)
+            mx_02.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[7].name;
+            track_artist.innerText = music_list[7].artist;
+            track_art.src = music_list[7].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_02;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_08(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[8].music);
+        loadTrack(8)
+        mx_03.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[8].name;
+        track_artist.innerText = music_list[8].artist;
+        track_art.src = music_list[8].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_03; //change
+        element_name = element_id.id
+    } else {
+        mx_03.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx3') { //change
+            curr_track = new Audio(music_list[8].music);
+            loadTrack(8)
+            mx_03.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[8].name;
+            track_artist.innerText = music_list[8].artist;
+            track_art.src = music_list[8].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_03;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_09(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[9].music);
+        loadTrack(9)
+        mx_04.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[9].name;
+        track_artist.innerText = music_list[9].artist;
+        track_art.src = music_list[9].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_04; //change
+        element_name = element_id.id
+    } else {
+        mx_04.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx4') { //change
+            curr_track = new Audio(music_list[9].music);
+            loadTrack(9)
+            mx_04.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[9].name;
+            track_artist.innerText = music_list[9].artist;
+            track_art.src = music_list[9].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_04;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_10(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[10].music);
+        loadTrack(10)
+        mx_05.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[10].name;
+        track_artist.innerText = music_list[10].artist;
+        track_art.src = music_list[10].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_05; //change
+        element_name = element_id.id
+    } else {
+        mx_05.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx5') { //change
+            curr_track = new Audio(music_list[10].music);
+            loadTrack(10)
+            mx_05.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[10].name;
+            track_artist.innerText = music_list[10].artist;
+            track_art.src = music_list[10].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_05;  //change
+            element_name = element_id.id;
+        }
+    }
+}
+
+function mxp_11(){
+    if (isPlaying == false) {
+        curr_track = new Audio(music_list[11].music);
+        loadTrack(11)
+        mx_06.setAttribute('name', 'pause'); //change
+        track_name.innerText = music_list[11].name;
+        track_artist.innerText = music_list[11].artist;
+        track_art.src = music_list[11].img;
+        playpauseTrack();
+        isPlaying = true;
+        element_id = mx_06; //change
+        element_name = element_id.id
+    } else {
+        mx_06.setAttribute('name', 'play');  //change
+        playpauseTrack();
+        isPlaying = false;
+        element_id = '';
+
+        if (element_name != 'mx6') { //change
+            curr_track = new Audio(music_list[11].music);
+            loadTrack(11)
+            mx_06.setAttribute('name', 'pause');    //change
+            track_name.innerText = music_list[11].name;
+            track_artist.innerText = music_list[11].artist;
+            track_art.src = music_list[11].img;
+            playpauseTrack();
+            isPlaying = true;
+            element_id = mx_06;  //change
+            element_name = element_id.id;
+        }
+    }
+}
--- app/js/script.js
+++ app/js/script.js
@@ -30,10 +30,10 @@ document.getElementById("volume_inp").addEventListener('mouseleave',  function()
     this.style.background = 'linear-gradient(to right, #fff 0%, #fff ' + value + '%, #7d7d7d ' + value + '%, #7d7d7d 100%)'
 });
 
-// function seek(){
-//     var seek = document.getElementById("seek_slider");
-//     var value = (seek.value-this.min)/(this.max-this.min)*100
-// }
+function seek(){
+    var seek = document.getElementById("seek_slider");
+    var value = (seek.value-this.min)/(this.max-this.min)*100
+}
 
 // document.getElementById("seek_slider").addEventListener('mouseover',  function() {
 //     var value = (this.value-this.min)/(this.max-this.min)*100
@@ -47,6 +47,94 @@ document.getElementById("volume_inp").addEventListener('mouseleave',  function()
 //     var value = (this.value-this.min)/(this.max-this.min)*100
 //     this.style.background = 'white'
 // });
+
+const music_list = [
+    {
+        img : 'images/songs/erandi.jpg',
+        name : 'Mamini Mamini Ma Deyya',
+        artist : 'Erandi Heshani',
+        music : 'music/0_mamini.mp3'
+    },
+    {
+        img : 'images/songs/calu p.jpg',
+        name : 'Manusath Kule',
+        artist : 'Kasun Gunasekara',
+        music : 'music/1_manusath.mp3'
+    },
+    {
+        img : 'images/songs/sansarini.jpg',
+        name : 'Sansarini',
+        artist : 'Yasas Medagedara',
+        music : 'music/2_sansarini.mp3'
+    },
+    {
+        img : 'images/songs/kan thuje.jpg',
+        name : 'KAUN TUJHE',
+        artist : 'Amaal Mallik, Palak Muchhal',
+        music : 'music/3_thuje.mp3'
+    },
+    {
+        img : 'images/songs/sandawathiye.jpg',
+        name : 'Sandawathiye',
+        artist : 'Charitha Attalage',
+        music : 'music/4_sandawathiye.mp3'
+    },
+    {
+        img : 'images/songs/baby shark.jpg',
+        name : 'Baby Shark',
+        artist : 'Robin Davies',
+        music : 'music/5_baby shark.mp3'
+    },
+    {
+        img : 'images/songs/inductryBaby.jpg',
+        name : 'INDUSTRY BABY (feat. Jack Harlow)',
+        artist : 'Lil Nas X, Jack Harlow',
+        music : 'music/6_INDUSTRY BABY.mp3'
+    },
+    {
+        img : 'images/songs/adk.jpg',
+        name : 'ADK and Yohani - Rider Fighter',
+        artist : 'ADK, Yohani',
+        music : 'music/7_Rider-Fighter.mp3'
+    },
+    {
+        img : 'images/songs/butter.jpg',
+        name : 'BTS (방탄소년단) "Butter"',
+        artist : 'BTS',
+        music : 'music/8_BTS.mp3'
+    },
+    {
+        img : 'images/songs/cardi b up.jpg',
+        name : 'Cardi B - Up (Official)',
+        artist : 'Cardi B',
+        music : 'music/8_Cardi B - Up (Official).webm'
+    },
+    {
+        img : 'images/songs/rap sellama.jpg',
+        name : 'Rap Sellama Mashup (Vol.02)',
+        artist : 'Desawana Remix',
+        music : 'music/9_Rap Sellama Mashup (Vol.02).mp3'
+    },
+    {
+        img : 'images/songs/rude boy.jpg',
+        name : 'Rihanna - Rude Boy (Official)',
+        artist : 'Rihanna',
+        music : 'music/10_Rihanna - Rude Boy (Official).mp3'
+    },
+    {
+        img : 'images/songs/sarith & surith.jpg',
+        name : 'Sinhala Punjabi Medley',
+        artist : 'Sarith Surith & NEWS',
+        music : 'music/11_Sinhala Punjabi Medley.mp3'
+    },
+    {
+        img : 'images/songs/umariya.jpg',
+        name : 'Jiya Re - Umariya ft. News',
+        artist : 'Umariya, NEWS',
+        music : 'music/12_Jiya Re - Umariya ft. News.mp3'
+    },
+];
+
 
 let top_01 = document.getElementById('top_01');
 let top_02 = document.getElementById('top_02');
@@ -83,24 +171,11 @@ let isPlaying = false;
 let isRandom = false;
 let updateTimer;
 
-const music_list = [
-    {
-        img : 'images/songs/inductryBaby.jpg',
-        name : 'INDUSTRY BABY (feat. Jack Harlow)',
-        artist : 'Lil Nas X, Jack Harlow',
-        music : 'music/INDUSTRY BABY.mp3'
-    },
-    {
-        img : 'images/songs/inductryBaby.jpg',
-        name : 'INDUSTRY BABY (feat. Jack Harlow)',
-        artist : 'Lil Nas X, Jack Harlow',
-        music : 'music/INDUSTRY BABY.mp3'
-    }
-];
-
 loadTrack(track_index);
 
 // All Play Functions
+let element_id = '';
+let element_name = '';
 
 
 function loadTrack(track_index){
@@ -110,7 +185,7 @@ function loadTrack(track_index){
     curr_track.src = music_list[track_index].music;
     curr_track.load();
 
-    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_art.style.backgroundImage = "images/songs/erandi.jpg";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
     // now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
@@ -148,16 +223,21 @@ function playpauseTrack(){
 function playTrack(){
     curr_track.play();
     isPlaying = true;
-    track_art.classList.add('rotate');
+    // track_art.classList.add('rotate');
     // wave.classList.add('loader');
     playpause_btn.setAttribute('name', 'pause-circle');
 }
 function pauseTrack(){
     curr_track.pause();
     isPlaying = false;
-    track_art.classList.remove('rotate');
+    // track_art.classList.remove('rotate');
     // wave.classList.remove('loader');
     playpause_btn.setAttribute('name', 'play-circle');
+    if (element_id != '') {
+        element_id.setAttribute('name', 'play');
+        element_id = ''; 
+    }
+
 }
 function nextTrack(){
     if(track_index < music_list.length - 1 && isRandom === false){
@@ -169,6 +249,9 @@ function nextTrack(){
         track_index = 0;
     }
     loadTrack(track_index);
+    track_name.innerText = music_list[track_index].name;
+    track_artist.innerText = music_list[track_index].artist;
+    track_art.src = music_list[track_index].img;
     playTrack();
 }
 function prevTrack(){
@@ -178,6 +261,9 @@ function prevTrack(){
         track_index = music_list.length -1;
     }
     loadTrack(track_index);
+    track_name.innerText = music_list[track_index].name;
+    track_artist.innerText = music_list[track_index].artist;
+    track_art.src = music_list[track_index].img;
     playTrack();
 }
 function seekTo(){
@@ -204,8 +290,8 @@ function setUpdate(){
         if(durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
 
         curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationMinutes;
-
-
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
+
+playRandom()
